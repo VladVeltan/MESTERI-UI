afde173398871cfac7d19f16c18abae9cb604ad2
VladVeltan <91504476+VladVeltan@users.noreply.github.com>
2024-04-06 17:32:42 +0300

    fetch and display listings

--- src/app/pages/listings-page/listings-page.component.ts
+++ src/app/pages/listings-page/listings-page.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListingService } from '../../servicies/listing.service';
+import { NgFor, NgIf } from '@angular/common';
+import { Listing } from '../../types/listing.types';
 
 @Component({
   selector: 'app-listings-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor,NgIf],
   templateUrl: './listings-page.component.html',
   styleUrl: './listings-page.component.scss'
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent  implements OnInit {
+  listings: Listing[] = [];
 
+  constructor(private listingService: ListingService) { }
+
+  ngOnInit(): void {
+    this.getListings();
+  }
+
+  getListings(): void {
+    this.listingService.getAllListings().subscribe(
+      (listings: any[]) => {
+        this.listings = listings;
+        console.log(listings)
+      },
+      (error) => {
+        console.error('Error fetching listings:', error);
+      }
+    );
+  }
 }
